79096222dd9824bd78b782d03775f61f68d8d4cf
emrecoban <fbdarktole@gmail.com>
2023-05-05 22:26:00 +0300

    Implemented routing

--- src/app/(back)/admin/auth/[to]/page.js
+++ src/app/(back)/admin/auth/[to]/page.js
@@ -1,7 +1,9 @@
 import LoginPage from "./LoginPage"
+import PasswordPage from "./PasswordPage"
+import RegisterPage from "./RegisterPage"
 
 export default function Auth({ params }) {
     if (params.to === "login") return (<LoginPage />)
-    if (params.to === "register") return (<LoginPage />)
-    if (params.to === "password") return (<LoginPage />)
+    if (params.to === "register") return (<RegisterPage />)
+    if (params.to === "password") return (<PasswordPage />)
 }
