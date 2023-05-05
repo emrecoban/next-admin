1397333d935cea602d297a958ccfd55173b61383
emrecoban <fbdarktole@gmail.com>
2023-05-05 23:26:01 +0300

    Added next/redirect function

--- src/app/(back)/admin/auth/[to]/page.js
+++ src/app/(back)/admin/auth/[to]/page.js
@@ -1,9 +1,23 @@
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { headers, cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
 import LoginPage from "./LoginPage"
 import PasswordPage from "./PasswordPage"
 import RegisterPage from "./RegisterPage"
 
-export default function Auth({ params }) {
-    if (params.to === "register") return (<RegisterPage />)
-    if (params.to === "password") return (<PasswordPage />)
-    return (<LoginPage />);
+export default async function Auth({ params }) {
+    const supabase = createServerComponentSupabaseClient({
+        headers,
+        cookies,
+    })
+    const { data: userData } = await supabase.auth.getSession();
+    if (!userData.session) {
+        if (params.to === "register") return (<RegisterPage />)
+        if (params.to === "password") return (<PasswordPage />)
+        return (<LoginPage />)
+    } else {
+        return redirect('/admin')
+    }
+
 }
