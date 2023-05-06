6d6a2622f10f1aae5a8dc1ebe006d030f6a90808
emrecoban <fbdarktole@gmail.com>
2023-05-07 02:16:04 +0300

    Updated layout and removed ex-middleware

--- src/app/(back)/admin/auth/layout.js
+++ src/app/(back)/admin/auth/layout.js
@@ -1,7 +1,17 @@
-export default function AuthLayout({ children }) {
-    return (
-        <>
-            {children}
-        </>
-    )
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { headers, cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function AuthLayout({ children }) {
+    const supabase = createServerComponentSupabaseClient({
+        headers,
+        cookies,
+    })
+    const { data: userData } = await supabase.auth.getSession()
+
+    if (userData.session) {
+        redirect('/admin')
+    }
+
+    return children
 }
