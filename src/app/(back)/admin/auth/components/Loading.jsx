0b04821385ede9a70f246354c8067336e52d3286
emrecoban <fbdarktole@gmail.com>
2023-05-08 15:37:47 +0300

    Loading component

--- src/app/(back)/admin/auth/components/Loading.jsx
+++ src/app/(back)/admin/auth/components/Loading.jsx
@@ -1,3 +1,9 @@
+import styles from './loading.module.css'
+
 export default function Loading() {
-    return <p>Yükleniyor...</p>
+    return (
+        <div className="p-5 md:px-16 md:py-12 flex items-center justify-center">
+            <div className={styles.loader}></div>
+        </div>
+    )
 }
