c767b72557f32fa35b34f019b37b77402dcb2c17
emrecoban <fbdarktole@gmail.com>
2023-05-08 20:22:46 +0300

    Added reset status to redirectAlert

--- src/app/(back)/admin/auth/login/page.jsx
+++ src/app/(back)/admin/auth/login/page.jsx
@@ -89,7 +89,7 @@ export default function LoginPage() {
                             )}
                             {/* END Alerts: Error */}
                             {/* Alerts: Success */}
-                            {(fromWhere == "register" || fromWhere == "password") && (
+                            {(fromWhere == "register" || fromWhere == "password" || fromWhere == "reset") && (
                                 <div className="p-4 md:p-5 mb-6 rounded text-emerald-700 bg-emerald-100">
                                     <div className="flex items-center mb-2">
                                         <svg className="hi-solid hi-check-circle inline-block w-5 h-5 mr-3 flex-none text-emerald-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
--- src/app/(back)/admin/auth/utils/redirectAlert.js
+++ src/app/(back)/admin/auth/utils/redirectAlert.js
@@ -10,6 +10,9 @@ export default function redirectAlert(where) {
     } else if (where === "password") {
         alert.title = "Sıfırlama e-postası gönderildi!"
         alert.message = "E-posta adresinize gelen şifre sıfırlama bağlantısına tıklayın."
+    } else if (where === "reset") {
+        alert.title = "Parola güncellendi!"
+        alert.message = "Parolanız başarılya güncellendi. Yeni parolanız ile giriş yapabilirsiniz."
     }
 
     return alert
